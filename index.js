13667ed9fc63bbd91e014abc286eb3f71ac977cf
IronBryce2011 <brycetboyd113@gmail.com>
2025-08-03 18:53:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -142,3 +142,7 @@ app.get('/api/announcement', async (req, res) => {
 });
 
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
